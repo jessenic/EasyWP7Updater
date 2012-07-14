7f6731b6c48793659efe5854ccc92fe4c5239cc6
jessenic <jessenic@digiex.net>
2012-07-14 18:11:58 +0300

    Added a very simple launcher

---
+++ EasyWP7UpdaterLauncher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EasyWP7UpdaterLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "";
+            try
+            {
+                bool is64bit = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"));
+                file = (is64bit ? "x86" : "x64") + @"\EasyWP7Updater.exe";
+                System.Diagnostics.Process.Start(file);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Failed to launch " + file + ": " + ex.Message + "\n\r\n\r\n\rFull error:\n\r"+ex.ToString(), "Error: " + ex.Message, System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+            }
+        }
+    }
+}
